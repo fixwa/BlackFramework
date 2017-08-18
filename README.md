78fe70d26d4f8e0ae6e1e5c9983e6e0b2ae5f919
Pablo <fixwah@gmail.com>
2017-08-18 11:39:55 -0300

    Fixes documentation for #9 Also improved the dump file a little bit

--- README.md
+++ README.md
@@ -10,12 +10,20 @@ The idea behind this is to have a lightweight basecode with some of the most imp
 
 ##Install
 
-Clone this reposotory into a local folder, then simply start a php webserver:
+Clone this reposotory into a local folder and run the following:
 
     cd BlackFramework
-    php -s localhost:8000
+    composer install
 
-browse to http://localhost:8000 and voilá!
+If you have mysql command line installed, run the command below, otherwise use your MySQL client:
+
+    mysql mysql -u {username} -p < localhost.sql
+
+Fire a webserver:
+
+    php -S localhost:8000
+
+Then browse to http://localhost:8000 and voilá!
 
 
 
--- localhost.sql
+++ localhost.sql
@@ -1,20 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.4
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-09-2014 a las 05:03:49
--- Versión del servidor: 5.6.15-log
--- Versión de PHP: 5.5.8
+-- Tiempo de generación: 18-08-2017 a las 14:35:22
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 7.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Base de datos: `blackfw`
+-- Base de datos: `myapplication`
 --
-CREATE DATABASE IF NOT EXISTS `blackfw` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `blackfw`;
+CREATE DATABASE IF NOT EXISTS `myapplication` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `myapplication`;
 
 -- --------------------------------------------------------
 
@@ -22,6 +28,7 @@ USE `blackfw`;
 -- Estructura de tabla para la tabla `modules`
 --
 
+DROP TABLE IF EXISTS `modules`;
 CREATE TABLE IF NOT EXISTS `modules` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(300) NOT NULL,
@@ -35,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `modules` (
   `enabled` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `enabled` (`enabled`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -43,9 +50,10 @@ CREATE TABLE IF NOT EXISTS `modules` (
 -- Estructura de tabla para la tabla `news`
 --
 
+DROP TABLE IF EXISTS `news`;
 CREATE TABLE IF NOT EXISTS `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `imported_id` int(10) unsigned NOT NULL,
+  `imported_id` int(10) UNSIGNED NOT NULL,
   `title` varchar(1000) NOT NULL,
   `intro` text NOT NULL,
   `body` text NOT NULL,
@@ -55,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `news` (
   `image_comment` varchar(1200) NOT NULL DEFAULT '',
   `images` text,
   `placeholder` varchar(30) DEFAULT NULL,
-  `views` bigint(20) unsigned NOT NULL,
+  `views` bigint(20) UNSIGNED NOT NULL,
   `date_created` datetime NOT NULL,
   `date_modified` datetime NOT NULL,
   `enabled` tinyint(4) NOT NULL DEFAULT '1',
@@ -65,9 +73,8 @@ CREATE TABLE IF NOT EXISTS `news` (
   KEY `placeholder` (`placeholder`),
   KEY `region` (`region`),
   KEY `source` (`source`),
-  KEY `imported_id` (`imported_id`),
-  FULLTEXT KEY `region_2` (`region`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  KEY `imported_id` (`imported_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -75,11 +82,12 @@ CREATE TABLE IF NOT EXISTS `news` (
 -- Estructura de tabla para la tabla `news_regions`
 --
 
+DROP TABLE IF EXISTS `news_regions`;
 CREATE TABLE IF NOT EXISTS `news_regions` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(300) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -87,20 +95,28 @@ CREATE TABLE IF NOT EXISTS `news_regions` (
 -- Estructura de tabla para la tabla `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(600) NOT NULL DEFAULT 'User',
   `email` varchar(600) NOT NULL,
   `password` varchar(200) NOT NULL,
   `role` varchar(200) NOT NULL DEFAULT 'user',
   `image` varchar(300) DEFAULT NULL,
   `parameters` text NOT NULL,
-  `enabled` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `enabled` tinyint(3) UNSIGNED NOT NULL DEFAULT '1',
   `date_created` datetime NOT NULL,
   `date_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `enabled` (`enabled`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `image`, `parameters`, `enabled`, `date_created`, `date_modified`) VALUES
+(1, 'Administrator', 'admin@email.com', '12345678', 'admin', NULL, 'O:8:"stdClass":2:{s:1:"0";b:0;s:9:"lastLogin";O:8:"stdClass":3:{s:4:"date";s:24:"2017-08-18T14:26:39+0000";s:2:"ip";s:3:"::1";s:8:"uniqueId";s:20:"[uid_1]5996f91f05f11";}}', 1, '2017-08-18 11:25:34', '2017-08-18 11:26:39');
 
 -- --------------------------------------------------------
 
@@ -108,6 +124,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Estructura de tabla para la tabla `videos`
 --
 
+DROP TABLE IF EXISTS `videos`;
 CREATE TABLE IF NOT EXISTS `videos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(600) DEFAULT NULL,
@@ -115,4 +132,17 @@ CREATE TABLE IF NOT EXISTS `videos` (
   `date_created` datetime NOT NULL,
   `date_modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `news`
+--
+ALTER TABLE `news` ADD FULLTEXT KEY `region_2` (`region`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
